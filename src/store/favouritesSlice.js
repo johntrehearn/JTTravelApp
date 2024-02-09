915849c9ed98b30eda4cd38ccc3aa2fc4a2618d4
johntrehearn <johntrehearnbc@gmail.com>
2024-02-09 10:33:57 +0200

    with old action

--- src/store/favouritesSlice.js
+++ src/store/favouritesSlice.js
@@ -1,4 +1,5 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { stat } from 'original-fs';
 
 export const favouritesSlice = createSlice({
   name: "favourites",
@@ -7,11 +8,21 @@ export const favouritesSlice = createSlice({
   },
   reducers: {
     addFavourite(state, action) {
+      if (state.favourites.some((favourites) => favourites.name === action.payload)) 
+      state.favourites = [...state.favourites];
+    state.favourites = [...state.favourites, action.payload];
+  }, 
+
+    /* OLD VERSION
+    reducers: {
+    addFavourite(state, action) {
       if (state.favourites.includes(action.payload)) {
-        return;
+        return state.favourites;
+      } else {
+
+        state.favourites = [...state.favourites, action.payload];
       }
-      state.favourites = [...state.favourites, action.payload];
-    },
+    }, */
     clearFavourites(state, action) {
       state.favourites = [];
     },
