aa10b5f5e24d6136df7c5a6be74074931379c8ce
johntrehearn <johntrehearnbc@gmail.com>
2024-02-16 12:10:05 +0200

    Weather description and picture is displaying

--- src/routes/CountriesSingle.jsx
+++ src/routes/CountriesSingle.jsx
@@ -20,6 +20,8 @@ const CountriesSingle = () => {
 
   console.log(location);
 
+  //Creating a useEffect to handle our requests to the weather API
+
   useEffect(() => {
     axios
       .get(
@@ -37,13 +39,61 @@ const CountriesSingle = () => {
 
   }, [country.capital]);
 
+  console.log("Weather: ", weather);
 
 
+  //Handling the Loading Case
+
+  if (loading) {
+    return (
+      <Col className='text-center m-5'>
+        <Spinner
+          animation='border'
+          role='status'
+          className='center'
+          variant='info'
+        >
+          <span className='visually-hidden'>Loading...</span>
+        </Spinner>
+      </Col>
+    );
+  }
 
   return (
     <Container>
+      <Row className='m-5'>
+        <Col>
+          {""}
+          <Image
+            thumbnail
+            src={`https://source.unsplash.com/featured/1600x900?${country.name.common}`}
+          />
+        </Col>
+        <Col>
+          <h2 className="display-4">{country.name.common}</h2>
+          <h3>Capitsl {country.capital}</h3>
+          {!error && weather && (
+            <div>
+              <p>
+                Weather right now is <strong>{weather.main.temp}</strong>{" "}
+                degrees in {country.capital} and {""}
+                {weather.weather[0].description}
+              </p>
+              <img src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+                alt='weather icon'
+              />
+            </div>
+          )}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <Button variant='light' onClick={() => navigate("/countries")}>
+            Back to Countries
+          </Button>
+        </Col>
+      </Row>
 
-      <div>Single Country will be here</div>
     </Container>
   );
 };
