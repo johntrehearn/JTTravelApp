1a0677082319fc4d491aa8aac6d1778a11b7e365
johntrehearn <johntrehearnbc@gmail.com>
2024-02-16 11:48:54 +0200

    Weather Object is pulling in

--- src/routes/CountriesSingle.jsx
+++ src/routes/CountriesSingle.jsx
@@ -1,8 +1,48 @@
-import { Container } from "react-bootstrap";
+import axios from 'axios';
+import { useEffect, useState } from "react";
+import { useLocation, useNavigate } from 'react-router-dom';
+import { Button, Col, Container, Image, Row, Spinner } from 'react-bootstrap';
+
+
+
+
+
+
 
 const CountriesSingle = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  const country = location.state.country;
+
+  const [weather, setWeather] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(true);
+
+  console.log(location);
+
+  useEffect(() => {
+    axios
+      .get(
+        `https://api.openweathermap.org/data/2.5/weather?q=${country.capital}&units=metric&appid=7db693258e4634c591f2445652fcf153`
+      )
+      .catch((error) => {
+        console.log(error);
+        setError(true);
+      })
+      .then((result) => {
+        setWeather(result.data);
+        console.log("The weather is...", result.data);
+        setLoading(false);
+      });
+
+  }, [country.capital]);
+
+
+
+
   return (
     <Container>
+
       <div>Single Country will be here</div>
     </Container>
   );
