5efe4ac83695dbbe3158412e907699057b1e58de
johntrehearn <johntrehearnbc@gmail.com>
2024-02-16 13:12:53 +0200

    protected routes working

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,8 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { Provider } from "react-redux";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ProtectedRoute from './auth/ProtectedRoute';
 import Countries from "./routes/Countries";
 import CountriesSingle from "./routes/CountriesSingle";
 import Favourites from "./routes/Favourites";
@@ -13,7 +14,7 @@ import store from "./store/store";
 import Register from "./routes/Register";
 import Login from "./routes/Login";
 
-const theme = createTheme( {
+const theme = createTheme({
   palette: {
     primary: {
       main: "#3f51b5",
@@ -22,10 +23,10 @@ const theme = createTheme( {
       main: "#f50057",
     },
   },
-} );
+});
 
 function App() {
-  const router = createBrowserRouter( [
+  /* const router = createBrowserRouter([
     {
       path: "/",
       element: <Root />,
@@ -56,13 +57,44 @@ function App() {
         }
       ],
     },
-  ] );
+  ]); */
 
   return (
     <Provider store={store}>
       <LocalizationProvider dateAdapter={AdapterDayjs}>
         <ThemeProvider theme={theme}>
-          <RouterProvider router={router}></RouterProvider>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Root />}>
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route
+                  path='/countries'
+                  element={
+                    <ProtectedRoute>
+                      <Countries />
+                    </ProtectedRoute>}
+                />
+                <Route
+                  path='/favourites'
+                  element={
+                    <ProtectedRoute>
+                      <Favourites />
+                    </ProtectedRoute>
+                  }
+                />
+                <Route
+                  path='/countries/:single'
+                  element={
+                    <ProtectedRoute>
+                      <CountriesSingle />
+                    </ProtectedRoute>
+                  }
+                />
+              </Route>
+            </Routes>
+          </Router>
         </ThemeProvider>
       </LocalizationProvider>
     </Provider>
