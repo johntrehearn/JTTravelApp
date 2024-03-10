7956103bd5020102aa01cc6f799bab37381f0722
johntrehearn <johntrehearnbc@gmail.com>
2024-03-10 14:41:40 +0200

    refresh start

---
+++ src/App.jsx
@@ -0,0 +1,59 @@
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import ProtectedRoute from "./auth/ProtectedRoute";
+import Countries from "./routes/Countries";
+import CountriesSingle from "./routes/CountriesSingle";
+import Favourites from "./routes/Favourites";
+import Home from "./routes/Home";
+import Login from "./routes/Login";
+import Register from "./routes/Register";
+import Root from "./routes/Root";
+import store from "./store/store";
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#3f51b5",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
+
+function App() {
+  return (
+    <Provider store={store}>
+      <LocalizationProvider dateAdapter={AdapterDayjs}>
+        <ThemeProvider theme={theme}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<Root />}>
+                <Route index element={<Home />} />
+                <Route path="login" element={<Login />} />
+                <Route path="register" element={<Register />} />
+                <Route
+                  path="/countries"
+                  element={<ProtectedRoute component={Countries} />}
+                />
+                <Route
+                  path="/favourites"
+                  element={<ProtectedRoute component={Favourites} />}
+                />
+                <Route
+                  path="/countries/:single"
+                  element={<ProtectedRoute component={CountriesSingle} />}
+                />
+              </Route>
+            </Routes>
+          </Router>
+        </ThemeProvider>
+      </LocalizationProvider>
+    </Provider>
+  );
+}
+
+export default App;
---
+++ src/store/favouritesSlice.js
@@ -0,0 +1,52 @@
+import { createSlice } from "@reduxjs/toolkit";
+import {
+  addFavouriteToFirebase,
+  auth,
+  clearFavouritesFromFirebase,
+  removeFavouriteFromFirebase,
+} from "../auth/firebase";
+
+export const favouritesSlice = createSlice({
+  name: "favourites",
+  initialState: {
+    favourites: [],
+  },
+  reducers: {
+    getFavourites(state, action) {
+      state.favourites = action.payload;
+    },
+    addFavourite(state, action) {
+      if (state.favourites.some((fav) => fav === action.payload))
+        state.favourites = [...state.favourites];
+      state.favourites = [...state.favourites, action.payload];
+
+      const user = auth.currentUser;
+      if (user) addFavouriteToFirebase(user.uid, action.payload);
+    },
+    removeFavourite(state, action) {
+      const newArray = [...state.favourites];
+      newArray.splice(
+        newArray.findIndex((e) => e === action.payload),
+        1
+      );
+      state.favourites = [...newArray];
+
+      const user = auth.currentUser;
+      if (user) {
+        removeFavouriteFromFirebase(user.uid, action.payload);
+      }
+    },
+    clearFavourites(state) {
+      state.favourites = [];
+      const user = auth.currentUser;
+      if (user) {
+        clearFavouritesFromFirebase(user.uid);
+      }
+    },
+  },
+});
+
+export const { getFavourites, addFavourite, clearFavourites, removeFavourite } =
+  favouritesSlice.actions;
+
+export default favouritesSlice.reducer;
